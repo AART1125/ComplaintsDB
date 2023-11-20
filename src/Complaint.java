7b0eacbe3b01c353de3ee133d629c193a5eb49e7
amrdgzmn <133744228+amrdgzmn@users.noreply.github.com>
2023-11-20 10:42:14 +0800

    Update complaint, complaintman., added resolution

--- src/Complaint.java
+++ src/Complaint.java
@@ -17,13 +17,12 @@ public class Complaint {
     public int complaintid;
     public int complainant;
     public Date dateofcomplaintfiling;
-    public Date dateofoffense;
     public Type typeofcomplaint;
     public Status statusofcomplaint; 
     public String description;
     
     // fields for personnel complaint
-    public int adminincharge; // to be changed in update
+    public int adminincharge;
     public int personnelrespondent;
     public Personnel_Involved typeofpersonnelcomplaint;
     // fields for security personnel complaint
@@ -39,17 +38,15 @@ public class Complaint {
     // fields for maintainance infra complaint
     public int maintainincharge;
     
-    public String dbconn = "\"jdbc:mysql://localhost:3306/INSERTDBNAMEHERE?user=root&password=12345678&useTimezone=true&serverTimezone=UTC&useSSL=false\";";
+    public String dbconn = "jdbc:mysql://localhost:3306/dbapp?user=root&password=12345678&useTimezone=true&serverTimezone=UTC&useSSL=false";
     
-    
-    public Complaint(int complaintid, int complainant, String dateofcomplaintfiling, String dateofoffense, String typeofcomplaint, String personnelrequired, int complaintsubject, String statusofcomplaint, String description) {
-        this.complaintid = complaintid;
+    public Complaint(int complainant, String dateofcomplaintfiling, String typeofcomplaint, String personnelrequired, int complaintsubject, String description) {
+        this.complaintid = 0;
         this.complainant = complainant;
         this.dateofcomplaintfiling = Date.valueOf(dateofcomplaintfiling);
-        this.dateofoffense = Date.valueOf(dateofoffense);
-        this.statusofcomplaint = Status.valueOf(statusofcomplaint);
-        
-        //Type.valueOf("P")
+        this.statusofcomplaint = Status.F; // automatically set to filed
+        this.typeofcomplaint = Type.valueOf(typeofcomplaint);
+        this.description = description;
         
         if (typeofcomplaint.compareTo("I") == 0) {
             this.problematicinfrastructure = complaintsubject;
@@ -58,7 +55,16 @@ public class Complaint {
             this.typeofinfrastructurecomplaint = Personnel_Involved.valueOf(personnelrequired);
         } else if (typeofcomplaint.compareTo("P") == 0) {
             this.personnelrespondent = complaintsubject;
+            this.securityincharge = 0;
+            this.maintainincharge = 0;
             this.typeofpersonnelcomplaint = Personnel_Involved.valueOf(personnelrequired);
+            if (typeofpersonnelcomplaint.name().compareTo("S") == 0) {
+                this.securityrespondent = personnelrespondent;
+                this.maintainrespondent = 0;
+            } else if (typeofpersonnelcomplaint.name().compareTo("M") == 0) {
+                this.maintainrespondent = personnelrespondent;
+                this.securityrespondent = 0;
+            }
             this.problematicinfrastructure = 0;
         }
         
@@ -81,28 +87,49 @@ public class Complaint {
             while (rst.next()) {
                 complaintid = rst.getInt("newID");
             }
-            pstmt = conn.prepareStatement("INSERT INTO complaint (complaintid, complainant, dateofcomplaintfiling, dateofoffense, typeofcomplaint, statusofcomplaint, description) VALUE (?,?,?,?,?,?,?)");
+            pstmt = conn.prepareStatement("INSERT INTO complaint (complaintid, complainant, dateofcomplaintfiling, typeofcomplaint, statusofcomplaint, description) VALUE (?,?,?,?,?,?)");
             pstmt.setInt(1, complaintid);
             pstmt.setInt(2, complainant);
             pstmt.setDate(3, dateofcomplaintfiling);
-            pstmt.setDate(4 , dateofoffense);
-            pstmt.setString(5, typeofcomplaint.name());
-            pstmt.setString(6, Status.F.name());            // automatically set to filed when created
-            pstmt.setString(7, description);
+            pstmt.setString(4, typeofcomplaint.name());
+            pstmt.setString(5, Status.F.name());            // automatically set to filed when created
+            pstmt.setString(6, description);
             pstmt.executeUpdate();
             
             if (typeofcomplaint.name().compareTo("P") == 0) {
-                pstmt = conn.prepareStatement("INSERT INTO personnelcomplaint (complaintid, personnelrespondent, typeofpersonnelcomplaint) VALUE (?, ?, ?)");
+                pstmt = conn.prepareStatement("INSERT INTO personnelcomplaint (complaintid, personnelrespondent, typeofpersoncomplaint) VALUE (?, ?, ?)");
                 pstmt.setInt(1, complaintid);
                 pstmt.setInt(2, personnelrespondent);
                 pstmt.setString(3, typeofpersonnelcomplaint.name());
+                pstmt.executeUpdate();
+                if (typeofpersonnelcomplaint.name().compareTo("S") == 0) {
+                    pstmt = conn.prepareStatement("INSERT INTO securitypersoncomplaint (complaintid, securityrespondent) VALUE (?,?)");
+                    pstmt.setInt(1, complaintid);
+                    pstmt.setInt(2, securityrespondent);
+                    pstmt.executeUpdate();
+                } else if (typeofpersonnelcomplaint.name().compareTo("M") == 0) {
+                    pstmt = conn.prepareStatement("INSERT INTO maintainpersoncomplaints (complaintid, maintainrespondent) VALUE (?,?)");
+                    pstmt.setInt(1, complaintid);
+                    pstmt.setInt(2, maintainrespondent);
+                    pstmt.executeUpdate();
+                }
             } else if (typeofcomplaint.name().compareTo("I") == 0) {
-                pstmt = conn.prepareStatement("INSERT INTO infrastructurecomplaint (complaintid, personnelrespondent, typeofpersonnelcomplaint) VALUE (?, ?, ?)");
+                pstmt = conn.prepareStatement("INSERT INTO infrastructurecomplaint (complaintid, problematicinfrastructure, typeofinfracomplaint) VALUE (?, ?, ?)");
                 pstmt.setInt(1, complaintid);
-                pstmt.setInt(2, securityrespondent);
+                pstmt.setInt(2, problematicinfrastructure);
                 pstmt.setString(3, typeofinfrastructurecomplaint.name());
+                pstmt.executeUpdate();
+                if (typeofinfrastructurecomplaint.name().compareTo("S") == 0) {
+                    pstmt = conn.prepareStatement("INSERT INTO securityinfracomplaint (complaintid) VALUE (?)");
+                    pstmt.setInt(1, complaintid);
+                    pstmt.executeUpdate();
+                } else if (typeofinfrastructurecomplaint.name().compareTo("M") == 0) {
+                    pstmt = conn.prepareStatement("INSERT INTO maintaininfracomplaint (complaintid) VALUE (?)");
+                    pstmt.setInt(1, complaintid);
+                    System.out.println("hello");
+                    pstmt.executeUpdate();
+                }
             }
-            pstmt.executeUpdate();
             pstmt.close();
             conn.close();
             return 1;
@@ -113,7 +140,7 @@ public class Complaint {
         
     }
     
-    
+    /*
     public int create_security_personnel_complaint() {
         try {
             Connection conn;
@@ -124,15 +151,10 @@ public class Complaint {
             while (rst.next()) {
                 complaintid = rst.getInt("newID");
             }
-            pstmt = conn.prepareStatement("INSERT INTO complaint (complaintid, complainant, dateofcomplaintfiling, dateofoffense, typeofcomplaint, statusofcomplaint, description) VALUE (?,?,?,?,?,?,?)");
+            pstmt = conn.prepareStatement("INSERT INTO securitypersoncomplaint (complaintid, complainant, dateofcomplaintfiling, typeofcomplaint, statusofcomplaint, description) VALUE (?,?,?,?,?,?,?)");
             pstmt.setInt(1, complaintid);
             pstmt.setInt(2, complainant);
-            pstmt.setDate(3, dateofcomplaintfiling);
-            pstmt.setDate(4 , dateofoffense);
-            pstmt.setString(5, typeofcomplaint.name());
-            pstmt.setString(6, statusofcomplaint.name());
-            pstmt.setString(7, description);
-            
+           
             pstmt.executeUpdate();
             pstmt.close();
             conn.close();
@@ -140,11 +162,11 @@ public class Complaint {
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return 0;
-        }
-        
+        }    
     }
+    */
     
-     // if complaint is deemed invalid by personnel
+     // if complaint is deemed invalid by admin
     public int delete_complaint() {
         try {
             Connection conn;
@@ -200,6 +222,7 @@ public class Complaint {
             conn = DriverManager.getConnection(dbconn);
             
             if (statusofcomplaint.name().compareTo("F") == 1) { // if not filed, DO NOT ALLOW to change
+                conn.close();
                 return 0;
             }
             
@@ -240,23 +263,63 @@ public class Complaint {
             Connection conn;
             conn = DriverManager.getConnection(dbconn);
             
-            PreparedStatement pstmt = conn.prepareStatement("SELECT complaintid, complainant, dateofcomplaintfiling, dateofoffense, typeofcomplaint, statusofcomplaint, description FROM complaint WHERE complaintid=?");
+            PreparedStatement pstmt = conn.prepareStatement("SELECT complaintid, complainant, dateofcomplaintfiling, typeofcomplaint, statusofcomplaint, description FROM complaint WHERE complaintid=?");
             pstmt.setInt(1, complaintid);
             ResultSet rst = pstmt.executeQuery();
             
             while (rst.next()) {
                 complainant = rst.getInt("complainant");
                 dateofcomplaintfiling = rst.getDate("dateofcomplaintfiling");
-                dateofoffense = rst.getDate("dateofoffense");
                 typeofcomplaint = Type.valueOf(rst.getString("typeofcomplaint"));
                 statusofcomplaint = Status.valueOf(rst.getString("statusofcomplaint"));
                 description = rst.getString("description");
+            }
+            
+            if (typeofcomplaint.name().compareTo("P") == 0) {
+                pstmt = conn.prepareStatement("SELECT personnelrespondent, typeofpersoncomplaint FROM personnelcomplaint WHERE complaintid=?");
+                pstmt.setInt(1, complaintid);
                 
+                rst = pstmt.executeQuery();
+                while (rst.next()) {
+                    personnelrespondent = rst.getInt("personnelrespondent");
+                    typeofpersonnelcomplaint = Personnel_Involved.valueOf(rst.getString("typeofpersoncomplaint"));                   
+                } 
+                
+                if (typeofpersonnelcomplaint.name().compareTo("S") == 0) {
+                    pstmt = conn.prepareStatement("SELECT securityrespondent FROM securitypersoncomplaint WHERE complaintid=?");
+                    pstmt.setInt(1, complaintid);
+                    
+                    rst = pstmt.executeQuery();
+                    problematicinfrastructure = 0;
+                    while (rst.next()) {
+                        securityrespondent = rst.getInt("securityrespondent");
+                        maintainrespondent = 0;
+                    }
+                } else if (typeofpersonnelcomplaint.name().compareTo("M") == 0) {
+                    pstmt = conn.prepareStatement("SELECT maintainrespondent FROM maintainpersoncomplaint WHERE complaintid=?");
+                    pstmt.setInt(1, complaintid);
+                    
+                    rst = pstmt.executeQuery();
+                    personnelrespondent = 0;
+                    securityrespondent = 0;
+                    maintainrespondent = 0;
+                    while (rst.next()) {
+                        maintainrespondent = rst.getInt("maintainrespondent");
+                    }
+                }
+            } else if (typeofcomplaint.name().compareTo("I") == 0) {
+                pstmt = conn.prepareStatement("SELECT problematicinfrastructure, FROM infrastructurecomplaint WHERE complaintid=?");
+                pstmt.setInt(1, complaintid);
+                
+                rst = pstmt.executeQuery();
+                
+                while (rst.next()) {
+                    problematicinfrastructure = rst.getInt("problematicinfrastructure");
+                }
             }
             
             pstmt.close();
             conn.close();
-            
             return 1; 
         } catch (Exception e) {
             System.out.println(e.getMessage());
